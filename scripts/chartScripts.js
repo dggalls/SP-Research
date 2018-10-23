4545c37ecc832f350fa197d3187ae9fefeab3d53
dggalls <danny.ogallchoir3@mail.dcu.ie>
2018-10-23 16:20:39 +0100

    Checkpoint

---
+++ scripts/chartScripts.js
@@ -0,0 +1,51 @@
+function generateChart(chartLabels, chartData) {
+    var chart = new Chartist.Line('.ct-chart', {
+        labels: chartLabels,
+        series: [chartData]
+    }, {
+        low: 0,
+        showArea: true,
+        showGrid: false,
+        showPoint: false,
+        width: '480px',
+        height: '250px'
+    });
+
+    chart.on('draw', function (data) {
+        if (data.type === 'line' || data.type === 'area') {
+            data.element.animate({
+                d: {
+                    begin: 800 * data.index, //Length between each animation
+                    dur: 600, //Length of each animation
+                    from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+                    to: data.path.clone().stringify(),
+                    easing: Chartist.Svg.Easing.easeOutQuint
+                }
+            });
+        }
+    });
+}
+
+function getChartData(columnNames, allText, newData, locationName) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var tempData = new Map();
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            for (var j=0; j<headers.length; j++) {
+                tempData.set(headers[j],data[j]);
+            }
+            if(tempData.get("State") === locationName) {
+                // tempData.forEach(function (arrayItem) {
+                //     console.log(arrayItem);
+                // });
+                for(var k=0; k<columnNames.length; k++) {
+                    newData.push(parseFloat(tempData.get(columnNames[k])))
+                }
+            }
+        }
+    }
+}
--- scripts/mapScripts.js
+++ scripts/mapScripts.js
@@ -18,3 +18,38 @@ function loadLayer() {
     });
 }
 
+
+function assignVectorColours(dataArray) {
+    var maxValue = 130000;
+
+    // Calculate color for each state based on the unemployment rate
+    dataArray.forEach(function(row) {
+        var green = (row["Amount"] / maxValue) * 255;
+        var color = "rgba(" + 0 + ", " + green + ", " + 0 + ", 1)";
+        expression.push(row["STATE"], color);
+    });
+
+
+    // Last value is the default, used where there is no data
+    expression.push("rgba(0,0,0,0)");
+}
+
+
+function getCSVFloatColumnData(columnName, allText, newData) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var tempData = new Map();
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            for (var j=0; j<headers.length; j++) {
+                tempData.set(headers[j],data[j]);
+            }
+
+            newData.push({"STATE": tempData.get("State"), "Amount": parseFloat(tempData.get(columnName))})
+        }
+    }
+}
+
